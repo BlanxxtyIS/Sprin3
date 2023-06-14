1dcca57bc073e6ef595e6a75acf905a23d460d92
BlanxxtyIS <Blanxxtyios@mail.ru>
2023-06-15 00:54:40 +0300

    Выполнил базовую задачу. Добавил счетчик UILabel который отображает информацию о нажатии. Добавил кнопку UIButton, который непосредственно изменяет значение UILabel. Выполнил усложненную задачу. Создал дополнительно две кнопки "+"  - красного цвета и "-" синего цвета. Создал кнопку обнуления с иконкой, которая при нажатии обнуляет значение счетчика. Создал текстовое поле UITextView, в котором отображается история изменения значения счетчика. Текстовое поле соответствует всем указаниям, отображает всю информацию, не редактирует пользователем и скролится.

--- Sprin3/ViewController.swift
+++ Sprin3/ViewController.swift
@@ -6,14 +6,77 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var labelCount: UILabel!
+    
+    @IBOutlet weak var buttonPush: UIButton!
+    
+    @IBOutlet weak var buttonPlus: UIButton!
+    
+    @IBOutlet weak var buttonMinus: UIButton!
+    
+    @IBOutlet weak var resetTheValue: UIButton!
+    
+    @IBOutlet weak var textView: UITextView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textView.text = "История изменений: \n"
+        textView.isEditable = false
+        //textView.scrollRangeToVisible(textView.selectedRange )
     }
+   
+    
+    private var count: Int = 0
+    let date = Date()
+    let dateFormatter = DateFormatter()
+    
 
 
+    @IBAction func ButtonClick(_ sender: Any) {
+        count += 1
+        labelCount.text = "\(count)"
+        dateFormatter.dateFormat = "dd-MM HH:mm:ss"
+        let dateString = dateFormatter.string(from: date)
+        textView.text += "[\(dateString)]: значение изменено на +1 \n"
+    }
+    
+    @IBAction func buttonPlusPush(_ sender: Any) {
+        count += 1
+        labelCount.text = "\(count)"
+        dateFormatter.dateFormat = "dd-MM HH:mm:ss"
+        let dateString = dateFormatter.string(from: date)
+        textView.text += "[\(dateString)]: значение изменено на +1 \n"
+    }
+    
+    @IBAction func buttonMinusPush(_ sender: Any) {
+        dateFormatter.dateFormat = "dd-MM HH:mm:ss"
+        let dateString = dateFormatter.string(from: date)
+        if count > 0 {
+            count -= 1
+            labelCount.text = "\(count)"
+            textView.text += "[\(dateString)]: значение изменено на -1 \n"
+        } else {
+            count = 0
+            textView.text += "[\(dateString)]: попытка уменьшить значение счетчика ниже 0 \n"
+        }
+    }
+    
+    
+    @IBAction func resetCountToZero(_ sender: Any) {
+        count = 0
+        labelCount.text = "\(count)"
+        dateFormatter.dateFormat = "dd-MM HH:mm:ss"
+        let dateString = dateFormatter.string(from: date)
+        textView.text = "[\(dateString)]: значение сброшено \n"
+    }
+    
+    
+    
 }
 
